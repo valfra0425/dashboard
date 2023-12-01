ea12fd11200d716631c24a01787ece3d4fd36929
valfra0425 <valmirfrancisco1@outlook.com>
2023-12-01 12:05:24 -0300

    atividade concluida

--- app7.py
+++ app7.py
@@ -5,10 +5,12 @@ import pandas as pd
 from dash import Dash, dcc, html, dash_table
 import plotly.express as px
 
+# Contador usado para atualizar a url
 count = 1
+# Arrays onde vou armazenar valores da raspagem de dados
 quotes = []
 authors = []
-felling = []
+positivity = []
 while True:
 
     # Url onde vamos fazer a raspagem de dados
@@ -23,61 +25,73 @@ while True:
         html_content = response.content
         soup = BeautifulSoup(html_content, 'html.parser')
 
-        # verifica a quantidade de
+        # verifica a quantidade de divs
+        # observando a página web eu notei que quando se tem apenas 8 divs a paginação acaba
         no_quotes = soup.find_all('div')
         if len(no_quotes) == 8:
             break
 
+        # divs com quotes
         quotes_div = soup.find_all('div', class_="quote")
         for i in quotes_div:
-            # # acho todos as quotes com a class text
+            # acho todos as quotes com a class text e adiciono no array
             quote = (i.find('span', class_="text")).text
             quotes.append(quote)
-            # acho todos os smalls com a class author
+            # acho todos os smalls com a class author e adiciono no array
             author = (i.find('small', class_="author")).text
             authors.append(author)
-            # análise da quote
+            # análise da quote usando o TextBlob
             analise = TextBlob(quote)
             polaridade = analise.sentiment.polarity
-            felling.append(polaridade)
+            positivity.append(polaridade)
 
         count += 1
     # Se deu errado cod != 2000
     else:
         print("Falha, status:{}".format(response.status_code))
 
-data = {'quotes': quotes, 'authors': authors, 'positividade': felling}
+# criação de um dataframe com os dados raspados
+data = {'quotes': quotes, 'authors': authors, 'positividade': positivity}
 df = pd.DataFrame(data)
+
+# conseguindo a media da positividade das frases de cada author
+df_group = df.groupby("authors")["positividade"].mean().reset_index()
+# conseguindo a contagem de quoutes de cada author
+df_group["count"] = (df.groupby("authors")["quotes"].count().reset_index())["quotes"]
+
+# gráfico
+fig = px.scatter(df_group, x="positividade", y="authors", color='positividade', hover_data=["count"],
+                 hover_name="authors", height=600)
+# tirando visibilidade do eixo y
+fig.update_layout(yaxis=dict(visible=False))
 
 # Cria uma instância do aplicativo Dash.
 app = Dash(__name__)
 
 # layout onde adicionamos as coisas do dashboard
-app.layout = html.Div(children=[
+app.layout = html.Div(className="main_container", children=[
     # H1 html
-    html.H1(children='Dataframe de frases'),
+    html.H1(className="center", children='Dashboard dos resultados da raspagem de dados do site'
+                                         ' https://quotes.toscrape.com'),
     # gráfico feito acima
     html.Div(
         children=[
+            html.H2(className="center", children="Dataframe criado a partir dos resultados"),
             # Exibindo o Datatable do dataframe baixado
-            dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-
+            dash_table.DataTable(data=df_group.to_dict('records'), page_size=10),
         ]
     ),
     html.Div(
         children=[
+            html.H2(className="center", children="Gráfico criado a partir dos resultados"),
             # exibindo o gráfico do dataframe
             dcc.Graph(
                 id="scatter",
-                figure={}
+                figure=fig
             )
         ]
     )
 ])
-
-df = px.data.iris()
-fig = px.scatter(df, x="sepal_width", y="sepal_length", color='petal_length')
-fig.show()
 
 if __name__ == '__main__':
     app.run(debug=True)
